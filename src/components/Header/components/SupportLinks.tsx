858a665006dd73265f082cbc16c32895aca59cd5
Rizon <kye021@hotmail.co.uk>
2025-07-29 16:09:41 +0100

    update: navbar with support links

---
+++ src/components/Header/components/SupportLinks.tsx
@@ -0,0 +1,58 @@
+'use client';
+import { useState } from 'react';
+
+const SupportLinksDropdown = () => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
+    return (
+        <div className="relative flex items-center space-x-4 mr-2">
+            <button
+                className="text-white font-medium text-sm flex items-center space-x-1"
+                onClick={toggleDropdown}
+            >
+                <span>{dropdownOpen ? "Support" : 'Support'}</span>
+                <svg
+                    className="w-4 h-4 transform transition-transform"
+                    style={{ transform: dropdownOpen ? "rotate(180deg)" : "rotate(0deg)" }}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+                </svg>
+            </button>
+            {dropdownOpen && (
+                <div className="absolute right-0 mt-52 w-48 bg-gradient-to-tl from-grey-900 via-dark_gray to-black rounded-md shadow-lg z-10">
+                    <div className="py-2">
+                        <a
+                            href="/kb"
+                            className="block px-4 py-2 text-white hover:text-blue"
+                        >
+                            Knowledge Base
+                        </a>
+                        <a
+                            href="/status"
+                            className="block px-4 py-2 text-white hover:text-blue"
+                        >
+                            Service Status
+                        </a>
+                        <a
+                            href="https://billing.nodebyte.host/submitticket.php"
+                            target="_blank"
+                            className="block px-4 py-2 text-white hover:text-blue"
+                        >
+                            Support Tickets
+                        </a>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default SupportLinksDropdown;
